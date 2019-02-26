1070ea753dc8dfed107ee0d6468e4bb9390a8378
Anh Quan Nguyen <anh@flowtify.de>
2019-02-26 14:12:41 +0100

    Add Formik, with dirty form check

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,53 +1,154 @@
 // @flow
-import React, { useState } from 'react'
-import AddIcon from '@material-ui/icons/Add'
+import React, { useEffect } from 'react'
 import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
+import { Formik } from 'formik'
+import * as Yup from "yup"
+import Card from '@material-ui/core/Card'
+import { CardContent } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import CardActions from '@material-ui/core/CardActions'
 
-export function TodoForm({ handleSubmit }) {
+function useDirtyForm(onUnload, dirty){
 
-  const [title, setTitle] = useState("")
-  const [note, setNote] = useState("")
+  useEffect( ()=>{
+    console.log("addEventListener beforeunload...")
+    window.removeEventListener('beforeunload',onUnload)
+    window.addEventListener('beforeunload', onUnload)
+    return ()=>{
+      console.log("removeEventListener beforeunload...")
+      window.removeEventListener('beforeunload',onUnload)
+    }
+  },[dirty])
+}
+export const TodoNativeForm = props => {
+  const {
+    handleSubmit,
+    values: { title, note },
+    errors,
+    touched,
+    handleChange,
+    isValid,
+    dirty,
+    setFieldTouched,
+  } = props
 
-  function onSubmitClick(e) {
-    e.preventDefault()
-    handleSubmit(title, note)
-    setTitle("")
-    setNote("")
+  const change = (fieldName, e) => {
+    e.persist()
+    handleChange(e)
+    setFieldTouched(fieldName, true, false)
   }
 
+  function handleOnUnload(event){
+    if (dirty){
+      // Cancel the event as stated by the standard.
+      event.preventDefault()
+      // Chrome requires returnValue to be set.
+      event.returnValue = ''
+      return false
+    }
+
+    return false
+  }
+
+  useDirtyForm(handleOnUnload, dirty)
+
+  const handleChangeTitle = change.bind(null, "name")
+  const handleChangeNote = change.bind(null, "note")
+
   return (
-    <>
-      <form>
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="center"
-        >
-          <TextField
-            id="title"
-            label="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            margin="normal"
-          />
+    <form onSubmit={(params) => {
+      handleSubmit(params)
+    }}
+    >
+      <Card>
+        <CardContent>
+          <div>
+            <Typography color="textSecondary" gutterBottom>
+              What are you doing today?
+              {dirty && (
+                <>Dirty</>
+              )}
+            </Typography>
+          </div>
+          <div>
+            <TextField
+              id="title"
+              name="title"
+              helperText={touched.title ? errors.title : "(required)"}
+              error={touched.title && Boolean(errors.title)}
+              label="Title"
+              value={title}
+              onChange={handleChangeTitle}
+            />
+          </div>
+          <div>
+            <TextField
+              id="note"
+              name="note"
+              helperText={touched.note ? errors.note : "(optional)"}
+              error={touched.note && Boolean(errors.note)}
+              label="Note"
+              value={note}
+              onChange={handleChangeNote}
+            />
+          </div>
+        </CardContent>
 
-          <TextField
-            id="note"
-            label="Note"
-            value={note}
-            onChange={(e) => setNote(e.target.value)}
-            margin="normal"
-          />
-
-          <Button variant="contained" color="primary"  aria-label="Add" onClick={onSubmitClick}>
-            <AddIcon/> Save
+        <CardActions>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!isValid}
+          >
+            Submit
           </Button>
-        </Grid>
-      </form>
+        </CardActions>
+      </Card>
+    </form>
+  )
+}
 
-    </>
+const validationSchema = Yup.object({
+  title: Yup.string("Enter task title")
+  .required("Task title is required"),
+})
+
+
+export function TodoForm({ handleSubmit }) {
+  const initialValues = { title: "", note: "" }
+
+  const onSubmit = async (values, {setSubmitting, setErrors, setStatus, resetForm}) => {
+    console.log("onSubmit: ",values)
+    try {
+      resetForm(initialValues)
+      setStatus({success: true})
+      setSubmitting(false)
+      const {title, note} = values
+      handleSubmit(title, note)
+    } catch (error) {
+      setStatus({success: false})
+      setSubmitting(false)
+      setErrors({submit: error.message})
+    }
+  }
+
+  function handleBlur(e:any) {
+    console.log("handleBlur: ",e)
+  }
+
+  console.log("TODOFORM is rendered")
+  return (
+    <React.Fragment>
+      <Formik
+        render={props => <TodoNativeForm handleSubmit {...props}/>}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+        onReset={console.log("Formik reset")}
+        handleBlur={handleBlur}
+      />
+    </React.Fragment>
   )
 }
